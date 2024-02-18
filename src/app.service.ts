cabba0130e103130b6e1e4df35dd0522609a040c
Mehrdad Mehralian <dadwic0@gmail.com>
2024-02-18 21:49:26 +0300

    feat: call github api using octokit/core

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,33 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { ConfigService } from '@nestjs/config';
+import { Octokit } from '@octokit/core';
 
 @Injectable()
 export class AppService {
   constructor(private configService: ConfigService) {}
 
-  @Cron('* * * * * *')
-  handleLog() {
-    console.log('Called when the current second is 45');
-  }
-
-  @Cron(CronExpression.EVERY_10_SECONDS)
+  @Cron(CronExpression.EVERY_30_SECONDS)
   async handleCron() {
     try {
+      // GitHub REST API
+      const octokit = new Octokit({
+        auth: this.configService.get<string>('GH_TOKEN'),
+      });
+      const response = await octokit.request(
+        'GET /repos/{owner}/{repo}/contents/{path}',
+        {
+          owner: this.configService.get<string>('GH_OWNER'),
+          repo: this.configService.get<string>('GH_REPO'),
+          path: this.configService.get<string>('GH_PATH'),
+          mediaType: {
+            format: 'raw',
+          },
+        },
+      );
+
       // Get the TRY sell price
-      const response = await fetch(this.configService.get<string>('GH_URL'));
-      const data: { p: string } = await response.json();
+      const data = JSON.parse(response.data.toString());
       const p = parseInt(data.p.replace(',', '')) / 10;
       const price = p + parseInt(this.configService.get<string>('FEE'));
-      console.log({ price });
 
-      // await fetch(this.configService.get<string>('API_URL'), {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      //   body: JSON.stringify({ price }),
-      // });
+      await fetch(this.configService.get<string>('API_URL'), {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ price }),
+      });
     } catch (error) {
       console.log({ error });
     }
